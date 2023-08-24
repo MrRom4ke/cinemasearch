f9a1def186c4fb1d1957e6e45bb7455a91ebfed7
asd <asdqwerz2006@gmail.com>
2023-08-24 18:47:34 +0300

    Added register file

---
+++ app/controllers/register.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, flash
+from werkzeug.security import generate_password_hash
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        if request.form['name'] and len(request.form['Nick']) > 3 and request.form['email'] and request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = database.addUser(request.form['name'], request.form['Nick'], request.form['email'], hash)
+            if res:
+                flash('Зарегистрирован', 'success')
+        else:
+            flash('Не все данные введены верно', 'error')
+    return render_template('register.html', title='Registration')
