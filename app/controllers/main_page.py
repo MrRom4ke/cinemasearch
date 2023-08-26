edc639f269df3b5ddb6afed4e16385d020ed3d2b
Roman Tyumenev <mrrom4ke@Romans-MacBook-Pro.local>
2023-08-26 19:12:13 +0400

    Добавлен маршрут для обработчика register. Переименован обработчик для главной страницы

--- app/controllers/main_page.py
+++ app/controllers/main_page.py
@@ -4,8 +4,3 @@ from flask import render_template
 def index():
     """Обработчик главной страницы"""
     return render_template('index.html')
-
-
-def register():
-    """Обработчик страницы регистрации"""
-    return render_template('register.html')
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,5 @@
 def add_routes(app):
-    from ..controllers import index
+    from ..controllers import main_page, reg_and_auth
 
-    app.add_url_rule('/', view_func=index.index)
-    app.add_url_rule('/register', view_func=index.register)
+    app.add_url_rule('/', view_func=main_page.index)
+    app.add_url_rule('/register', view_func=reg_and_auth.register, methods=['POST', 'GET'])
