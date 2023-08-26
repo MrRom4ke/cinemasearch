ca5a0807e3cf2162b35f4dee437cfa03f376bc16
Roman Tyumenev <75197546+MrRom4ke@users.noreply.github.com>
2023-08-26 18:57:42 +0400

    Update and rename register.py to reg_and_auth.py
    Учел свои замечания, и исправил их

--- app/controllers/reg_and_auth.py
+++ app/controllers/reg_and_auth.py
@@ -1,15 +1,13 @@
 from flask import Flask, request, render_template, flash
 from werkzeug.security import generate_password_hash
 
-from CinemaSearch.cinemasearch.wsgi import app
-
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        if request.form['name'] and len(request.form['Nick']) > 3 and request.form['email'] and request.form['psw'] == request.form['psw2']:
+        if request.form['name'] and request.form['email'] and request.form['psw'] == request.form['psw2']:
             hash = generate_password_hash(request.form['psw'])
-            res = database.addUser(request.form['name'], request.form['Nick'], request.form['email'], hash)
+            res = database.addUser(request.form['name'], request.form['email'], hash)
             if res:
                 flash('Зарегистрирован', 'success')
         else:
