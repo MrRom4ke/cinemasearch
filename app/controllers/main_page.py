354489799dde636755436345a52c490fc44282df
asd <asdqwerz2006@gmail.com>
2023-09-03 15:24:59 +0300

    changes in main page

--- app/controllers/main_page.py
+++ app/controllers/main_page.py
@@ -3,4 +3,4 @@ from flask import render_template, g
 
 def index_view():
     """Обработчик главной страницы"""
-    return render_template('index.html', menu=g.menu_repo.get_menu(), title='Главная страница')
+    return render_template('index.html', menu=g.menu_repo.get_menu(), title='Главная страница', films=g.film_repo.get_film())
